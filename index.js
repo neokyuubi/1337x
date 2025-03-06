772f35352caab043eb1b062d02fd6e7ea5184b00
Neokyuubi <neokyuubi@gmail.com>
2025-03-06 01:32:15 +0000

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         1337x Magnet Link Fetcher
-// @version      1.0
+// @version      1.1
 // @description  Adds checkboxes and magnet link extraction functionality to 1337x.to search results
 // @author       neokyuubi
 // @match        *://1337x.to/search/*
